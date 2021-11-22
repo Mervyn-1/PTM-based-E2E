7c6798ff35aa479d2260d0b8dc359c73a65b813d
yanjunZheng <55747445+Mervyn-1@users.noreply.github.com>
2021-11-22 18:56:42 +0800

    Add files via upload

---
+++ Pretraining/pretraining_pptod_base.sh
@@ -0,0 +1,11 @@
+CUDA_VISIBLE_DEVICES=2,3,4,5 python pretrain.py\
+    --dataset_prefix_path ../data/pre-training_corpora/tokenized_pretraining_corpora/\
+    --model_name t5-base\
+    --batch_size_per_gpu 1\
+    --number_of_gpu 4\
+    --gradient_accumulation_steps 2\
+    --save_steps 5000\
+    --save_path ../checkpoints/\
+    --save_ckpt_name base \
+    --use_turn_info True \
+    --use_spans_prediction True
